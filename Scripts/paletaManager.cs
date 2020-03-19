6f888e133ff7ccd7cc1d4d1b72802a419f69946d
vselfa <vselfa@ieseljust.com>
2020-03-19 18:10:59 +0100

    Flippers, axis i inici script

--- Scripts/paletaManager.cs
+++ Scripts/paletaManager.cs
@@ -9,30 +9,86 @@ public class paletaManager : MonoBehaviour{
     public float pressedPosition = 45f;
     public float hitStrenght = 10000f;
     public float flipperDamper = 150f;
+    // Axis control
+    public string inputName;
+
     private HingeJoint hinge;
-    public int mitatPantalla;
+   
+    // To mobile devices
+    // public int mitatPantalla;
 
     // Start is called before the first frame update
     void Start()  {
         hinge = GetComponent<HingeJoint>();
         hinge.useSpring = true;
         mitatPantalla = Screen.width /2;
-
-        print ("Tag Inici");
     }
 
-    // Update is called once per frame
-    void Update() {
+     // Update is called once per frame
+    void Update()
+    {
         JointSpring spring = new JointSpring();
         spring.spring = hitStrenght;
         spring.damper = flipperDamper;
-
-        if (Input.GetKeyDown(KeyCode.A) )
+        print ("Tag Update: " + tag);
+        
+        // Mobile devices
+        /*if (Input.GetKeyDown(KeyCode.A) || Input.touchCount > 0)
         {
-			print ("Fletxes horitzontals" + tag);
-            spring.targetPosition = pressedPosition;      
+			print ("Tag KeyCode.A" + tag);
+
+            if (Input.GetTouch(0).position.x < mitatPantalla )
+            {
+                if (tag == "PaletaEsquerre")
+                {
+                    spring.targetPosition = pressedPosition;
+                    print ("Tag Esquerre");
+                }
+                else
+                {
+                    spring.targetPosition = restPosition;
+                }
+            }
+            if (Input.GetTouch(0).position.x >= mitatPantalla)
+            {
+                if (tag == "PaletaDreta")
+                {
+                    spring.targetPosition = pressedPosition;
+                    print ("Tag Dreta");
+
+                }
+                else
+                {
+                    spring.targetPosition = restPosition;
+                }
+            }
         }
+        else
+        {
+            spring.targetPosition = restPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
+        */
+
+        // Standalone PC
+        if (Input.GetAxis (inputName)== a)  {  // Left flipper
+
+
+
+
+        }
+        else { // Right flipper
+
+
+
+
+
+        }
+
+
 
 
     }
 }
+    
