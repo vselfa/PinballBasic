f450276ad7692a22e3ff844bee604a9598510136
vselfa <vselfa@ieseljust.com>
2020-03-19 20:36:11 +0100

    Flippers working

--- Scripts/paletaManager.cs
+++ Scripts/paletaManager.cs
@@ -2,26 +2,25 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-
 public class paletaManager : MonoBehaviour{
 
     public float restPosition = 0f;
     public float pressedPosition = 45f;
     public float hitStrenght = 10000f;
-    public float flipperDamper = 150f;
+    public float flipperDamper = 300f;
     // Axis control
-    public string inputName;
+    // public string inputName ="a";
 
     private HingeJoint hinge;
    
     // To mobile devices
-    // public int mitatPantalla;
+    // public int meitatPantalla;
 
     // Start is called before the first frame update
     void Start()  {
         hinge = GetComponent<HingeJoint>();
         hinge.useSpring = true;
-        mitatPantalla = Screen.width /2;
+        // meitatPantalla = Screen.width /2;
     }
 
      // Update is called once per frame
@@ -30,14 +29,37 @@ public class paletaManager : MonoBehaviour{
         JointSpring spring = new JointSpring();
         spring.spring = hitStrenght;
         spring.damper = flipperDamper;
-        print ("Tag Update: " + tag);
         
+        // Standalone PC
+        // See Edit -> Prject settings -> Inoput Manager
+        if (Input.GetAxis ("LeftFlipper") == 1)  {  // Left flipper activated with a
+            print ("a key: " + tag);
+            if (tag == "PaletaEsquerre") {
+                spring.targetPosition = pressedPosition;
+            }
+            else {
+                spring.targetPosition = restPosition;
+            }
+        }
+        // See Edit -> Prject settings -> Inoput Manager
+        if (Input.GetAxis ("RightFlipper") == 1) {  // Right flipper activated with l
+            if (tag == "PaletaDreta") {
+                spring.targetPosition = pressedPosition;
+            }
+            else {
+                spring.targetPosition = restPosition;
+            }
+        }
+
+        hinge.spring = spring;
+        hinge.useLimits = true;
+
         // Mobile devices
         /*if (Input.GetKeyDown(KeyCode.A) || Input.touchCount > 0)
         {
 			print ("Tag KeyCode.A" + tag);
 
-            if (Input.GetTouch(0).position.x < mitatPantalla )
+            if (Input.GetTouch(0).position.x < meitatPantalla )
             {
                 if (tag == "PaletaEsquerre")
                 {
@@ -49,7 +71,7 @@ public class paletaManager : MonoBehaviour{
                     spring.targetPosition = restPosition;
                 }
             }
-            if (Input.GetTouch(0).position.x >= mitatPantalla)
+            if (Input.GetTouch(0).position.x >= meitatPantalla)
             {
                 if (tag == "PaletaDreta")
                 {
@@ -70,23 +92,6 @@ public class paletaManager : MonoBehaviour{
         hinge.spring = spring;
         hinge.useLimits = true;
         */
-
-        // Standalone PC
-        if (Input.GetAxis (inputName)== a)  {  // Left flipper
-
-
-
-
-        }
-        else { // Right flipper
-
-
-
-
-
-        }
-
-
 
 
     }
